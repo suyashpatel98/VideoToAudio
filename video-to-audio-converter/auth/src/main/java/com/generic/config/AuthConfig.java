342e171023a45cf6405fbca5ff45ac7473c387b1
Suyash Patel <suyashpatel98@gmail.com>
2024-02-24 16:20:48 -0800

    Cleanup after being able to add a user

--- video-to-audio-converter/auth/src/main/java/com/generic/config/AuthConfig.java
+++
@@ -1,9 +0,0 @@
-package com.generic.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.generic")
-public class AuthConfig {
-}
